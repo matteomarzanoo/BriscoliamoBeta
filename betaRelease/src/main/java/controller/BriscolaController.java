e9b0d83c95c6a6c837ddbd391c4cb02565e79473
Nickbike03 <dtmncl03c24c349x@studenti.unical.it>
2024-06-10 18:34:09 +0200

    cioa

--- betaRelease/src/main/java/controller/BriscolaController.java
+++ betaRelease/src/main/java/controller/BriscolaController.java
@@ -58,7 +58,6 @@ public class BriscolaController extends KeyAdapter
                         Card card = game.getHandPlayer().get(i);
                         if (card.contains(e.getPoint())) {
                             playCard(i);
-
                         }
                     }
                 }
